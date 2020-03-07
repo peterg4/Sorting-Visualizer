ec7e6147a529eb5680590720c767613c6da9241e
peterg4 <43735968+peterg4@users.noreply.github.com>
2020-03-07 10:12:57 -0500

    intial board setup; variable len and number bars

--- sorter/src/App.js
+++ sorter/src/App.js
@@ -1,26 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+var numBars = 10;
+function Bar(props) {
+  var height = {
+    height: props.value,
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className = {'bar'} style={height}></div>
   );
+}
+
+class Area extends React.Component {
+  constructor(props) {
+    super(props);
+    var bars = [];
+    for(var i = 1; i < numBars; i++){
+      bars.push(i*10);
+    }
+    this.state = {
+      bars: bars,
+      length: bars.length,
+    };
+  }
+
+  renderBar(i) {
+    return (
+      <Bar
+        value = {this.state.bars[i]}
+      // bar attributes added here
+      />
+    );
+  }
+
+
+  render() {
+    const sorts = []
+    for(var i = 0; i < numBars-1; i++) {
+      sorts.push(this.renderBar(i));
+    }
+
+
+
+    return (
+      <div className="sortBoard">
+        {sorts}
+      </div>
+    )
+  }
+
+}
+
+class App extends React.Component {
+  render() {
+
+
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Area />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
