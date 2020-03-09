5f2c9c41b4a3100da10f486b83730bfd81b70b11
peterg4 <43735968+peterg4@users.noreply.github.com>
2020-03-09 10:14:52 -0400

    working mergesort-will refactor

--- sorter/src/App.js
+++ sorter/src/App.js
@@ -96,7 +96,6 @@ class Area extends React.Component {
     }, 5000/this.state.length)
   }
   selectionSort(){
-    //contiunally find the min and place at "beginning"
     var i = 0;
     var low = 0;
     var sort = setInterval(() => {
@@ -125,8 +124,69 @@ class Area extends React.Component {
       }
     }, 5000/this.state.length)
   }
+  merge(arr, l, m, r) {
+    console.log(arr, l ,m ,r);
+    var i,j,k;
+    var n1 = m-l+1;
+    var n2 = r-m;
+    var L = new Array(n1);
+    var R = new Array(n2);
+    for(var i = 0; i < n1; i++) {
+      L[i] = arr[l+i];
+    }
+    for(var j = 0; j < n2; j++) {
+      R[j] = arr[m+1+j];
+    }
+    console.log(L, R);
+    i=0;
+    j=0;
+    k=l;
+    while(i < n1 && j < n2) {
+      if(L[i] <= R[j]) {
+        arr[k] = L[i];
+        i++;
+      } else {
+        arr[k] = R[j];
+        j++;
+      }
+      k++;
+    }
+    while(i < n1) {
+      arr[k] = L[i];
+      i++;
+      k++;
+    }
+    while(j < n2) {
+      arr[k] = R[j];
+      j++;
+      k++;
+    }
+    console.log(arr);
+    return arr;
+  }
   mergeSort(){
     //decimate then merge
+    var curr_size;
+    var left_start;
+    var n = this.state.length;
+    curr_size = 1;
+    var arr = this.state.bars.slice();
+    var search = setInterval(() => {
+      console.log('teeeeee\n');
+      for(left_start=0; left_start<n-1;left_start+=2*curr_size) {
+        var mid = Math.min(left_start + curr_size-1,n-1);
+        var right_end = Math.min(left_start+(2*curr_size)-1,n-1);
+        arr = this.merge(arr,left_start,mid,right_end);
+        console.log('_______________________________________');
+      }
+      this.setState({bars: arr});
+      curr_size*=2;
+      if(curr_size > n-1) {
+        console.log('tes');
+        clearInterval(search);
+        this.confirmSort();
+      }
+    }, 10000/this.state.length)
   }
   partition(arr, low, high) {
     var x = arr[high];
