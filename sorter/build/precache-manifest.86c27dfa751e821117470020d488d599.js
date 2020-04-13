f344579658c5ffe75ac161382bd78816c02584e4
peterg4 <43735968+peterg4@users.noreply.github.com>
2020-04-13 00:22:30 -0400

    build files

---
+++ sorter/build/precache-manifest.86c27dfa751e821117470020d488d599.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35e3a60907984742c9ae5078f7bd362f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fff164fa3c1da4bd7ce",
+    "url": "/static/css/main.481c395e.chunk.css"
+  },
+  {
+    "revision": "4b137e9dd373d6277d50",
+    "url": "/static/js/2.2b238a5a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.2b238a5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fff164fa3c1da4bd7ce",
+    "url": "/static/js/main.9dd961fe.chunk.js"
+  },
+  {
+    "revision": "3455f4a0c2485cce041d",
+    "url": "/static/js/runtime-main.6e41c95d.js"
+  }
+]);
